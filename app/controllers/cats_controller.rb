01065d143711f37c8814a38e245355dc7d18dc00
LEARN Academy <ford.py@gmail.com>
2022-02-07 13:25:17 -0800

    Api-Endpoints complete

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,3 +1,11 @@
 class CatsController < ApplicationController
-    
+    def index
+        cats = Cat.all
+        render json: cats
+    end
+
+    def create
+        cat = Cat.create(cat_params)
+        render json: cat
+    end
 end
