c46d9131d596a8287ae56ec0cfe9c723527b08b5
LEARN Academy <ford.py@gmail.com>
2022-02-07 16:15:02 -0800

    fixed failing tests

--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -18,14 +18,25 @@ RSpec.describe "Cats", type: :request do
       expect(cat.length).to eq 1
     end
   end 
-    describe"POST /create" do
+    describe "POST /create" do
       it 'creates a cat' do
-        cat_params ={
+        cat_params = {
+          cat: {
           name: 'Felix',
           age: 2,
-          enjoys: 'Walks in the park' ,
+          enjoys: 'walk in the park' ,
           image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+          }
         }
+
+        post '/cats', params: cat_params
+        expect(response).to have_http_status(200)
+
+        cat = Cat.first
+        expect(cat.name).to eq('Felix')
+        expect(cat.age).to eq(2)
+        expect(cat.enjoys).to eq('walk in the park')
+        expect(cat.image).to eq('https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80')
 
       end
     end
